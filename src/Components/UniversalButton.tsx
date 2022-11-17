f650934bf43f0e83c377a9dc55b62cb6a62c6130
lgnvmaksim <lgnvmaksim@gmail.com>
2022-11-17 18:23:52 +0300

    get name for UniversalButton

--- src/Components/UniversalButton.tsx
+++ src/Components/UniversalButton.tsx
@@ -1,11 +1,13 @@
 type ButtonType = {
     callback: () => void
     styles?: string
+    name:string
 }
 
 
 export const UniversalButton = (props: ButtonType) => {
     const onClickHandler = () => props.callback()
     const stylesButton = props.styles
-    return <button className={stylesButton} onClick={onClickHandler}></button>
+    return <button className={stylesButton}
+                   onClick={onClickHandler}>{props.name}</button>
 }
