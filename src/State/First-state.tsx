04839740afeddca1c17c5480fd18fafbc39f5897
lgnvmaksim <lgnvmaksim@gmail.com>
2022-11-17 15:42:08 +0300

    commit before start

---
+++ src/State/First-state.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// let [tasks, setTasks] = useState([
+//     {id: v1(), title: "HTML&CSS", isDone: true},
+//     {id: v1(), title: "JS", isDone: true},
+//     {id: v1(), title: "ReactJS", isDone: false},
+//     {id: v1(), title: "Rest API", isDone: false},
+//     {id: v1(), title: "GraphQL", isDone: false},
+// ]);
