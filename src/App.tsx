eb0c5179d6b6c3bfe56b50e714d216d879f98f6c
lgnvmaksim <lgnvmaksim@gmail.com>
2022-11-17 18:22:28 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,40 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 import {v1} from "uuid";
-import s from './Components/Styles-modules/styles.module.css'
+import a from './Components/Styles-modules/styles.module.css'
 
+type FilteredType='all'| 'active'| 'completed'
+export type TaskType={
+    id:string
+    title:string
+    isDone:boolean
+}
 
 function App() {
 
+    let [tasks, setTasks] = useState<TaskType[]>([
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false},
+        {id: v1(), title: "GraphQL", isDone: false},
+    ]);
+    const [filter, setFilter] =useState<FilteredType>('all')
 
-    return <div className={s.App}>
-        <Todolist titleValue={'Hello ToDo'}/>
+    let filteredTasks = tasks
+    if (filter ==='active') {
+        filteredTasks=tasks.filter(f=>!f.isDone)
+    }
+    if (filter ==='completed') {
+        filteredTasks=tasks.filter(f=>f.isDone)
+    }
+
+
+    return <div className={a.App}>
+        <Todolist titleValue={'Hello ToDo'}
+                  tasks={filteredTasks}
+        />
     </div>
-
 
 }
 
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,18 +1,26 @@
-import s from './Components/Styles-modules/styles.module.css'
+import a from './Components/Styles-modules/styles.module.css'
+import {TaskType} from "./App";
+
 
 type TodolistPropsType={
     titleValue:string
+    tasks:TaskType[]
 }
 
 export const Todolist = (props:TodolistPropsType) => {
-    return <div className={s.todolist}>
+    return <div className={a.todolist}>
         <h3>{props.titleValue}</h3>
         <input/>
         <button>+</button>
         <ul>
-            <li>
-                <span>Hello</span>
-            </li>
+            {props.tasks.map(el=>{
+                return(
+                    <li key={el.id}>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                    </li>
+                )
+            })}
         </ul>
         <button>All</button>
         <button>Active</button>
