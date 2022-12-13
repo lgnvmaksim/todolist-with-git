e2cca538dc47ffbc7d28f1d1cfb05a09905f8c7d
lgnvmaksim <lgnvmaksim@gmail.com>
2022-12-13 14:32:58 +0300

    Merge all project into Master branch

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ function App() {
             {id: v1(), title: "JS2", isDone: true},
             {id: v1(), title: "ReactJS2", isDone: false},
             {id: v1(), title: "Rest API2", isDone: false},
-            {id: v1(), title: "GraphQL2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false}
         ]
     });
 
